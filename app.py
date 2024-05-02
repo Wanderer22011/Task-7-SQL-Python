bece5113c10fc62c2ff6a7195c07b0afe555be9e
Wanderer22011 <22011@burnside.school.nz>
2024-05-02 19:35:26 +1200

    Added database and more code

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print('Hello, World!')
+import sqlite3
+
+db=sqlite3.connect("Task7Database.db")
+cursor= db.cursor()
+sql="SELECT* FROM Planes;"
+cursor.execute(sql)
+results=cursor.fetchall()
+for i in results:
+    print(i)
+db.close()
